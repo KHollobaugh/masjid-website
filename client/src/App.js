1ae1dfdd12cfee604134240d32de19b560f7e4ef
KHollobaugh <khollobaugh@gmail.com>
2019-01-02 22:18:17 -0500

    components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Route} from 'react-router-dom';
+import './css/index.css';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Footer from './components/Footer';
 import './App.css';
+
+document.body.classList.add('app-background');
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+      <Route exact path = '/' component = {Home} />
+      <Route path = '/contact' component = {Contact} />
+
+      <Footer />
       </div>
     );
   }
